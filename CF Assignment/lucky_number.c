f7b5709557ddacfa2695bb200ffbd6362dac28c1
koemnak <heatkimnak@gmail.com>
2022-07-20 15:53:34 +0700

    lucky number

---
+++ CF Assignment/lucky_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a ;
+	printf("Enter the number :");
+	scanf("%d",&a);
+	if (a%2==0){
+		printf("Divisible 2 by not 3!");
+	}else if(a%3==0){
+		printf("Divisible by 3 not 2!");
+	}else{
+		
+	}
+	return 0;
+	
+	
+	
+}
