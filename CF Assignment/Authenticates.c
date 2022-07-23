503c8d99a5ccee2bb605722daf3fb19d4f85bd1c
koemnak <heatkimnak@gmail.com>
2022-07-23 19:38:21 +0700

    access to someone's password by uername

---
+++ CF Assignment/Authenticates.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int password,John;
+	printf("Enter Your Password : ");
+	scanf("%d",&password);
+	if (password==John){
+		if (password!=John){
+			printf("Access Denied!");
+		}else{
+			printf("Program Terminal!");	
+		}
+		
+	}else if (password==John){
+		printf("Access is Granted");
+	}else{
+		printf("Wellcome to RUPP system.");
+	}
+	return 0;
+	
+}
